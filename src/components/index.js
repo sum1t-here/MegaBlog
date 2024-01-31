484e553f49a891b32846eb14f05a39c474c8e152
sumit <mazumdarsumit3@gmail.com>
2024-01-31 13:56:08 +0530

    add: production grade react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Headers from './Headers/Headers';
 import Footers from './Footers/Footers';
+import Container from './Containers/Container';
+import Logo from './Logo';
 
-export { Headers, Footers };
+export { Headers, Footers, Container, Logo };
