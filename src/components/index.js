c3190c21e4d3adc9980f5c3d4d9b089ee4c2c8c7
sumit <mazumdarsumit3@gmail.com>
2024-02-08 00:08:22 +0530

    Add: RTE and post-form

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Headers from './Headers/Headers';
 import Footers from './Footers/Footers';
 import Container from './Containers/Container';
 import Logo from './Logo';
+import RTE from './RTE';
 
-export { Headers, Footers, Container, Logo };
+export { Headers, Footers, Container, Logo, RTE };
