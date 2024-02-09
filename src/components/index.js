9ceddb62be2f19fccf729c5ba7d347633c7b5f9c
sumit <mazumdarsumit3@gmail.com>
2024-02-09 18:17:14 +0530

    Add: Done with pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,19 @@ import Footers from './Footers/Footers';
 import Container from './Containers/Container';
 import Logo from './Logo';
 import RTE from './RTE';
+import Signup from './Signup';
+import Login from './Login';
+import PostForm from './Post-form';
+import PostCard from './PostCard';
 
-export { Headers, Footers, Container, Logo, RTE };
+export {
+  Headers,
+  Footers,
+  Container,
+  Logo,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+};
